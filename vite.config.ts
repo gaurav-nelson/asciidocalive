c4f48c1537ca52df03a7c4f1011fd092db85278b
Gaurav Nelson <23069445+gaurav-nelson@users.noreply.github.com>
2024-11-20 13:15:47 +1000

    Use asciidoctor CSS, better import export

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+    },
+  },
   preview: {
     port: 8080,
     strictPort: true,
